5e2f194ecaf421ef787d6845e137fea0187f2fcb
Rekha <Rekha.Kushwaha@ibm.com>
2023-01-06 13:27:22 +0530

    Assignment2-commit

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const heading1 = React.createElement(
   "h1",
   {
     id: "head1",
   },
-  "First Heading"
+  "First Heading with parcel"
 );
 
 const heading2 = React.createElement(
